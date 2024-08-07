bec0aaa5e7d9369ba553e5615f811619bfec5e5d
Fernando Martinez-Urrutia <89673693+mr-nahash@users.noreply.github.com>
2024-08-07 00:58:51 -0600

    trying changig connection to mongodb

--- mysite/list_of_apps/views.py
+++ mysite/list_of_apps/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 
 # Create your views here.
-from django.shortcuts import render
 
 def collection_data(request):
-    client = MongoClient('mongodb+srv://u_pick_it:u_pick_it@chemconnect.mbuwkso.mongodb.net/?retryWrites=true&w=majority&appName=ChemConnect&tls=true&tlsAllowInvalidCertificates=true')
+    uri = "mongodb+srv://u_pick_it:u_pick_it@chemconnect.mbuwkso.mongodb.net/?retryWrites=true&w=majority&appName=ChemConnect"
+
+    #    Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))    
     db = client['chemConnect']
     collection = db['references']
     
-    data = collection.find()  # Retrieve all documents in the collection
-    return render(request, 'list_of_apps/collection_data.html', {'data': data})
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        data = collection.find()  # Retrieve all documents in the collection
+        return render(request, 'list_of_apps/collection_data.html', {'data': data})
+
+    except Exception as e:
+        print(e)
