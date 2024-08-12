dd95abc2d45f59abbc08760f04e8e2c6724911fe
Fernando Martinez-Urrutia <89673693+mr-nahash@users.noreply.github.com>
2024-08-12 00:23:05 -0600

    last working session

--- next-js-front/src/app/page.tsx
+++ next-js-front/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import ToolCard from "@/components/AppListing/CardSSR";
+import ToolCard from "@/components/AppListing/ToolCard";
 
 
 export default async function HomePage() {
